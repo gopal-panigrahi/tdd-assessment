07f2bf928832d496f6f3411fb3de08c76aec746c
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-11-27 19:17:25 +0530

    added scenario to handle custom delimiters

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,7 +1,13 @@
-const possible_delimiters = /[\,\n]/;
-
 function string_calculator(numbers) {
-  const numbers_arr = numbers.split(possible_delimiters).filter((n) => !!n);
+  let number_string = numbers;
+  let delimiters = /[\,\n]/;
+
+  if (numbers.startsWith("//")) {
+    [delimiters, number_string] = numbers.split("\n");
+    delimiters = delimiters.substring(2);
+  }
+
+  const numbers_arr = number_string.split(delimiters).filter((n) => !!n);
   const sum = numbers_arr.reduce((acc, val) => acc + parseInt(val), 0);
   return sum;
 }
--- src/string_calculator.test.js
+++ src/string_calculator.test.js
@@ -20,4 +20,9 @@ describe("string calculator", () => {
     const result = string_calculator("1\n2,3");
     expect(result).toBe(1 + 2 + 3);
   });
+
+  test("when string is passed with custom delimiter", () => {
+    const result = string_calculator("//;\n1;2");
+    expect(result).toBe(1 + 2);
+  });
 });
